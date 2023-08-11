325de015937edf271c8782fd61f084ed2b66a68e
arsmourtuis <vicvalvila@gmail.com>
2023-08-10 21:44:59 -0600

    last details

--- src/Components/Inicio/Header.jsx
+++ src/Components/Inicio/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
     {/* HEADER */}
     <nav class="navbar navbar-expand-lg navbar-light header">
-        <div class="container-fluid flex-row">
+        <div class="container-fluid">
 
             <a class="navbar-brand" href="#">Inicio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
