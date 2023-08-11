38e6e898a741712ecdcc7524950f7776d2d730f1
arsmourtuis <vicvalvila@gmail.com>
2023-08-10 21:39:01 -0600

    Hola si

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import UserProfile from "./Components/UserProfile";
 import Productos from "./Components/Productos/Productos";
+import Header from "./Components/Inicio/Header";
 // Componente principal App
 function App() {
   return (
     <div>
+      <Header/>
       <Productos/>
     </div>
   );
