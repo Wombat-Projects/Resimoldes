6261b31ccdbbf166e0c1de78bd5d6254b2fa2657
arsmourtuis <vicvalvila@gmail.com>
2023-08-10 19:10:43 -0600

    header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import UserProfile from "./Components/UserProfile";
 import Productos from "./Components/Productos/Productos";
+import Header from "./Components/Inicio/Header";
 // Componente principal App
 function App() {
   return (
     <div>
-      <Productos/>
+      <Header />
+      {/* <Productos/> */}
     </div>
   );
 }
---
+++ src/Components/Inicio/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Inicio.css";
+
+const Header = () => {
+    return (
+    <> 
+    <div className="container-principal">
+        <div>   </div>
+        <div className="logo"> <img src="https://i.imgur.com/t6pkRLW.png" alt="wombat logo" className='logo' /> </div>  
+        <div className='Logo-login'> <a href="#"><ion-icon name="person" size="large"></ion-icon></a></div>
+    </div>
+
+    {/* HEADER */}
+    <nav class="navbar navbar-expand-lg navbar-light header">
+        <div class="container-fluid flex-row">
+
+            <a class="navbar-brand" href="#">Inicio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>   
+
+            <div class="collapse navbar-collapse justify-content-start" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    </>
+
+    )
+}
+
+export default Header
