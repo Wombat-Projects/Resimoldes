5643d05e68cc97f0f55e141ee6125a6d37ef0ffd
xKeros <johanquiros968@gmail.com>
2023-08-25 20:30:56 -0600

    Add: Rutas v1

--- src/Components/Inicio/Header.jsx
+++ src/Components/Inicio/Header.jsx
@@ -1,45 +1,55 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Inicio.css";
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
-    <> 
-    <div className="container-principal">
-        <div> </div>
-        <div className="logo"> <h1> Wombatmoldes</h1> </div>  
-        <div className='Logo-login'> <a href="/"><ion-icon name="person"  size="large"></ion-icon></a></div>
-    </div>
-
-    {/* HEADER */}
-    <nav class="navbar navbar-expand-lg navbar-light header">
-        <div class="container-fluid">
-
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>   
-
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <a class="navbar-brand hover-underline" href="/home">Inicio</a>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link hover-underline" aria-current="page" href="/products">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link hover-underline" href="/about">Sobre Nosotros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link hover-underline" href="/contact">Contacto</a>
-                    </li>
-                </ul>
+        <> 
+        <div className="container-principal">
+            <div> </div>
+            <div className="logo"> <h1> Wombatmoldes</h1> </div>  
+            <div className='Logo-login'> 
+                {/* Usa la función navigate en lugar de <a> */}
+                <button onClick={() => navigate("/")}>
+                    <ion-icon name="person" size="large"></ion-icon>
+                </button>
             </div>
         </div>
-    </nav>
 
-    
-    </>
+        {/* HEADER */}
+        <nav className="navbar navbar-expand-lg navbar-light header">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>   
 
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <button className="navbar-brand hover-underline" onClick={() => navigate("/home")}>
+                        Inicio
+                    </button>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <button className="nav-link hover-underline" onClick={() => navigate("/products")}>
+                                Productos
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link hover-underline" onClick={() => navigate("/about")}>
+                                Sobre Nosotros
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link hover-underline" onClick={() => navigate("/contact")}>
+                                Contacto
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </>
     )
 }
 
-export default Header
+export default Header;
