24b8f8dc1530d0fc48295d652e0b3bb39d23dd69
xKeros <johanquiros968@gmail.com>
2023-08-25 22:39:52 -0600

    Add: Secciones en especial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "../Components/Inicio/Header";
 import Slider from "../Components/Inicio/Slider";
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const hash = location.hash;
+    if (hash === '#seccionEspecifica') {
+      const element = document.getElementById('seccionEspecifica');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location.hash]);
   return (
     <div>
       <Header />
       <Slider />
+      <div id="seccionEspecifica">
+        <h1>Hola</h1>
+      </div>
     </div>
   );
 };
