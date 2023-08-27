beb49d104dd11563fadb95bf5e06e501576b3e92
arsmourtuis <vicvalvila@gmail.com>
2023-08-26 21:54:59 -0600

    new cha

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../Components/Inicio/Header";
 import Slider from "../Components/Inicio/Slider";
+import Footer from "../Components/Inicio/Footer";
+import Cuadro from "../Components/Inicio/Cuadro";
+import Forms from "../Components/Inicio/forms-contactos";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Slider />
+      <Cuadro />
+      <Forms />
+      <Footer />
     </div>
   );
 };
